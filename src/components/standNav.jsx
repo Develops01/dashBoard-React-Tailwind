c67f7ce524bde58450d853706238c7db0aceba6e
mohammad <ronins1375@gmail.com>
2023-12-21 16:04:16 +0330

    stand-nav-iconFix

--- src/components/standNav.jsx
+++ src/components/standNav.jsx
@@ -36,15 +36,27 @@ const StandNav = () => {
         className="lg:hidden fixed top-7 right-7  shadow-sm shadow-[#BE6779] dark:shadow-white rounded-full p-1 z-50 cursor-pointer"
         onClick={toggleMobileNav}
       >
-        <svg
-          className="dark:text-white text-[#D4AF37]  fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 512 512"
-        >
-          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-        </svg>
+        {isMobileNavOpen ? (
+          <svg
+            className="dark:text-white text-[#D4AF37]  fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        ) : (
+          <svg
+            className="dark:text-white text-[#D4AF37]  fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+        )}
       </button>
 
       {/* desktop Navigation */}
