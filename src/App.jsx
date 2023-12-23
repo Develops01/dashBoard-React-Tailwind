181a95cab0d19934939f1294b8ce9e19591bba5b
mohammad <ronins1375@gmail.com>
2023-12-23 16:44:08 +0330

    style-blog

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,11 @@ function App() {
     <>
       <TopNav />
       <StandNav />
-      <div className="h-screen sm:pt-24 pt-20 dark:bg-slate-700  flex justify-center items-center ">
+      <div className="flex-col h-screen sm:pt-24 pt-20 dark:bg-slate-700  flex justify-center items-center ">
         <UserTable />
-      </div>
-      <div className="h-screen sm:pt-24 pr-28 pt-20 dark:bg-slate-700  flex justify-center items-center ">
         <BlogForm />
       </div>
+      <div className="h-screen sm:pt-24 pr-28 py-96 dark:bg-slate-700  flex justify-center items-center "></div>
 
       <Footer />
     </>
--- src/components/blogForm.jsx
+++ src/components/blogForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-
+import { faTrash, faTag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const BlogForm = () => {
   const [selectedTags, setSelectedTags] = useState(new Set());
+  const [selectedTags2, setSelectedTags2] = useState(new Set());
   const [tagInput, setTagInput] = useState("");
   const [tagList, setTagList] = useState([]);
 
@@ -18,9 +20,9 @@ const BlogForm = () => {
   };
   const addTag2 = () => {
     if (tagInput.trim() !== "") {
-      const newSelectedTags2 = new Set(selectedTags);
+      const newSelectedTags2 = new Set(selectedTags2);
       newSelectedTags2.add(tagInput);
-      setSelectedTags(newSelectedTags2);
+      setSelectedTags2(newSelectedTags2);
 
       const newTagList = [...tagList, tagInput];
       setTagList(newTagList);
@@ -42,15 +44,17 @@ const BlogForm = () => {
   };
 
   return (
-    <div className="overflow-x-auto lg:w-4/5 w-11/12 p-10 my-10 shadow-md rounded-lg h-fit flex flex-col">
-      <p className="mb-5 border-b p-2 text-xl">افزودن بلاگ جدید</p>
+    <div className="  h-fit p-10 my-10 shadow-md shadow-sky-200 dark:bg-slate-700 dark:shadow-slate-900 rounded-lg  flex flex-col">
+      <p className=" dark:text-white mb-5 border-b p-2 text-xl">
+        افزودن بلاگ جدید
+      </p>
 
-      <div className="gap-2 items-center justify-start flex">
+      <div className=" gap-2 items-center justify-start flex">
         <div className="dropdown mt-3">
-          <div className="flex gap-3 flex-col justify-start items-start">
+          <div className="dark:text-white flex gap-3 flex-col justify-start items-start">
             <label htmlFor="tagSelector">یک دسته بندی انتخاب کن:</label>
             <select
-              className="p-3 shadow bg-gray-800 rounded-box w-auto"
+              className="bg-white dark:text-white border shadow-sky-200 dark:border-gray-800 dark:shadow-slate-800 dark:bg-slate-800  p-3 cursor-pointer shadow-md rounded-lg focus:outline-none  rounded-box w-auto"
               id="tagSelector"
             >
               <option value="" disabled selected>
@@ -63,18 +67,25 @@ const BlogForm = () => {
               <option value="ادمین">ادمین</option>
               <option value="بازار">بازار</option>
             </select>
-            <button className="btn btn-outline btn-md" onClick={addTag}>
+            <button
+              className="dark:text-white border shadow-md shadow-sky-200 dark:border-gray-800 dark:shadow-slate-800 dark:bg-slate-800 rounded-md
+              px-6 py-3"
+              onClick={addTag}
+            >
               اضافه کردن دسته
             </button>
           </div>
         </div>
 
-        <div className="tag-container flex flex-wrap" id="tagContainer">
+        <div
+          className="dark:text-white tag-container flex flex-wrap"
+          id="tagContainer"
+        >
           {/* Display selected tags */}
           {[...selectedTags].map((tag) => (
             <div
               key={tag}
-              className="border rounded inline-block max-w-fit px-3 m-1 text-base break-words"
+              className="border dark:bg-slate-800 rounded inline-block max-w-fit px-4 py-1  m-1 text-base break-words"
             >
               {tag}
               <button
@@ -85,18 +96,23 @@ const BlogForm = () => {
                   tagSelector.options.namedItem(tag).disabled = false;
                 }}
               >
-                X
+                <FontAwesomeIcon
+                  className=" mr-2 text-rose-600"
+                  icon={faTrash}
+                />
               </button>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="flex justify-around align-center mt-5 gap-2">
-        <div className="w-1/2">
-          <label htmlFor="test name">تیتر بلاگ :</label>
+      <div className=" flex justify-around align-center mt-5 gap-2">
+        <div className=" w-1/2">
+          <label className="dark:text-white" htmlFor="blogLabel">
+            تیتر بلاگ :
+          </label>
           <input
-            name="test name"
+            id="blogLabel"
             className="w-full border rounded px-3 py-2 mt-2"
             type="text"
             required
@@ -104,9 +120,11 @@ const BlogForm = () => {
         </div>
 
         <div className="w-1/2">
-          <label htmlFor="video time">زمان مطالعه :</label>
+          <label className="dark:text-white" htmlFor="TimeToRead">
+            زمان مطالعه :
+          </label>
           <input
-            name="video time"
+            id="TimeToRead"
             className="w-full border rounded px-3 py-2 mt-2"
             type="text"
             required
@@ -114,30 +132,32 @@ const BlogForm = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-start items-start w-full mt-5">
-        <label htmlFor="summary">خلاصه متن:</label>
+      <div className=" flex flex-col justify-start items-start w-full mt-5">
+        <label className="dark:text-white" htmlFor="summary">
+          خلاصه متن:
+        </label>
         <textarea
-          htmlFor="summary"
+          id="summary"
           placeholder="خلاصه متن"
           className="border rounded w-full px-3 py-2 mt-2"
         ></textarea>
       </div>
 
-      <div className="flex justify-around align-center mt-5 gap-2">
-        <div className="w-1/2">
-          <label htmlFor="upload video">پیوند ویدیو :</label>
+      <div className="dark:text-white flex justify-around align-center mt-5 gap-2">
+        <div className=" w-1/2">
+          <label htmlFor="uploadVid">پیوند ویدیو :</label>
           <input
-            name="upload video"
-            className="border rounded w-full px-3 py-2 mt-2"
+            id="uploadVid"
+            className=" border rounded w-full px-3 py-2 mt-2"
             type="file"
             required
           />
         </div>
 
         <div className="w-1/2">
-          <label htmlFor="upload img">پیوند تصویر :</label>
+          <label htmlFor="uploadImg">پیوند تصویر :</label>
           <input
-            name="video img"
+            id="uploadImg"
             className="border rounded w-full px-3 py-2 mt-2"
             type="file"
             required
@@ -145,20 +165,24 @@ const BlogForm = () => {
         </div>
       </div>
 
-      <div className="w-full mt-5">
-        <label htmlFor="namek">نامک :</label>
+      <div className=" w-full mt-5">
+        <label className="dark:text-white" htmlFor="naamak">
+          نامک :
+        </label>
         <input
-          name="namek"
+          id="naamak"
           className="w-full border rounded px-3 py-2 mt-2"
           type="text"
           required
         />
       </div>
 
-      <div className="flex flex-col justify-start items-start w-full mt-5">
-        <label htmlFor="content">متن بلاگ:</label>
+      <div className=" flex flex-col justify-start items-start w-full mt-5">
+        <label className="dark:text-white" htmlFor="blogContent">
+          متن بلاگ:
+        </label>
         <textarea
-          htmlFor="content"
+          id="blogContent"
           placeholder=" متن بلاگ"
           className="border rounded w-full px-3 py-2 mt-2"
         ></textarea>
@@ -166,27 +190,36 @@ const BlogForm = () => {
 
       <div className="w-full mt-5 flex flex-col justify-center items-center">
         <div className="w-full flex justify-between items-center">
-          <p className="text-xl">پرسش ها</p>
-          <button className="btn btn-outline btn-md">افزودن پرسش</button>
+          <p className="dark:text-white text-xl">پرسش ها</p>
+          <button
+            className="dark:text-white border shadow-md shadow-sky-200 dark:border-gray-800 dark:shadow-slate-800 dark:bg-slate-800 rounded-md
+             px-7 py-3"
+          >
+            افزودن پرسش
+          </button>
         </div>
 
         <div className="w-full flex flex-col justify-center mt-5">
-          <label htmlFor="question">متن پرسش :</label>
+          <label className="dark:text-white" htmlFor="question">
+            متن پرسش :
+          </label>
           <input
-            name="question"
+            id="question"
             className="w-full border rounded px-3 py-2 mt-2"
             type="text"
             required
           />
 
           <div className="w-full mt-5">
-            <label htmlFor="myInput">افزودن برچسب :</label>
+            <label className="dark:text-white" htmlFor="addLabel">
+              افزودن برچسب :
+            </label>
             <div className="relative w-full rounded-lg">
               <input
                 className="border rounded w-full px-3 py-2 mt-2"
                 placeholder="  ثبت با کلید ENTER"
                 type="text"
-                id="myInput"
+                id="addLabel"
                 value={tagInput}
                 onChange={(e) => setTagInput(e.target.value)}
                 onKeyUp={(e) => {
@@ -195,26 +228,35 @@ const BlogForm = () => {
                   }
                 }}
               />
-              <i className="fa fa-tags absolute top-1/2 left-3 -translate-y-1/2"></i>
             </div>
-            <div className="tags">
+            <div>
               {tagList.map((tag, index) => (
                 <span
                   key={index}
-                  className="border rounded inline-block max-w-fit px-3 m-1 text-base break-words"
+                  className=" dark:bg-slate-800 dark:text-white border rounded inline-block max-w-fit px-3 py-1 m-1 text-base break-words"
                 >
-                  # {tag}
-                  <i
-                    className="fa fa-solid fa-times cursor-pointer transition-all duration-1000 hover:bg-rose-600 rounded hover:text-red-200 m-1 py-1 text-rose-600 text-xl p-2"
+                  <FontAwesomeIcon
+                    className="ml-1 text-amber-400"
+                    icon={faTag}
+                  />{" "}
+                  {tag}
+                  <FontAwesomeIcon
                     onClick={() => deleteTag(index)}
-                  ></i>
+                    className=" mr-3 text-rose-600 cursor-pointer"
+                    icon={faTrash}
+                  />
                 </span>
               ))}
             </div>
           </div>
 
           <div className="flex justify-start mt-5 w-full">
-            <button className="btn btn-outline btn-md">ذخیره</button>
+            <button
+              className="dark:text-white border shadow-md shadow-sky-200 dark:border-gray-800 dark:shadow-slate-800 dark:bg-slate-800 rounded-md
+             px-7 py-3"
+            >
+              ذخیره
+            </button>
           </div>
         </div>
       </div>
